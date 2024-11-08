836422bbafe81afcc1b1446cbe96911d2035f308
Jens Laufer <jens.laufer@solytics.de>
2024-11-08 09:27:09 +0100

    net value 2024-11-07

--- src/content.js
+++ src/content.js
@@ -1,5 +1,10 @@
-const btn = document.createElement("BUTTON")
-const t = document.createTextNode("CLICK ME");
-btn.appendChild(t);
+// const btn = document.createElement("BUTTON")
+// const t = document.createTextNode("CLICK ME");
+// btn.appendChild(t);
 
-document.body.appendChild(btn);
+// document.body.appendChild(btn);
+
+const iframe = document.createElement("iframe");
+//iframe.style.display = "none";
+iframe.src = chrome.extension.getUrl("index.html/#/frame");
+document.body.appendChild(iframe);
