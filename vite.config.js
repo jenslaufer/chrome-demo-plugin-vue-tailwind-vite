1dfe4def988618f594325c8c470ea7a75e3a528e
Jens Laufer <jens.laufer@solytics.de>
2024-11-14 16:45:00 +0100

    removed obsolete code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' assert { type: 'json' }
-import zipPack from "vite-plugin-zip-pack";
+import manifest from './manifest.json'
+import zipPack from "vite-plugin-zip-pack"
 
 
 export default defineConfig({
